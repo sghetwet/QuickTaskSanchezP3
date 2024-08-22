f18e05ee5c21aa1f8a040b3a0206192eec2bd874
Adam Sanchez <145504826+sghetwet@users.noreply.github.com>
2024-08-22 11:35:01 -0700

    Added most of the level and player controls
    made the player able to move left right, and jump, and added a stage

--- QuickTaskP3/Assets/Scripts/Movement.cs
+++ QuickTaskP3/Assets/Scripts/Movement.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using static UnityEditor.Timeline.TimelinePlaybackControls;
 
 public class Movement : MonoBehaviour
 {
+    public float horizontal;
+    public float speed;
+    public float jump;
+    private float Move;
+    public Text winText;
+    Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move = Input.GetAxisRaw("Horizontal");
+
+        rb.velocity = new Vector2(speed = Move, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(new Vector2(rb.velocity.x, jump));
+        }
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "win")
+        {
+            winText.gameObject.SetActive(true);
+        }
+
     }
 }
