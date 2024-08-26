d7780f7718e342215006f490af7292545534d474
Adam Sanchez <145504826+sghetwet@users.noreply.github.com>
2024-08-26 11:11:20 -0700

    My completed Quick Task
    Idk how I managed to complete this but I did and I suck at coding :)

--- QuickTaskP3/Assets/Scripts/GameControl.cs
+++ QuickTaskP3/Assets/Scripts/GameControl.cs
@@ -2,14 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class GameControl : MonoBehaviour
 {
-    public Text winText;
+    public TextMeshProUGUI winText;
     // Start is called before the first frame update
     void Start()
     {
-        
+        winText.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
